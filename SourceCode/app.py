6fd88e0f4f1c69e117ec5c41f0530889d6180cb9
Andrzej Szlaski <242545@edu.p.lodz.pl>
2024-01-05 18:21:57 +0100

    deleted unnecessary files, project for grade 4 finalised, preparation for grade 5 started including only making appropriate html elements considering this part of assignment

--- SourceCode/app.py
+++ SourceCode/app.py
@@ -1,5 +1,5 @@
-import requests
-from flask import Flask, render_template, redirect, request, url_for, abort, jsonify
+# import requests
+from flask import Flask, render_template, redirect, request, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 from jsonschema import validate, ValidationError
 
@@ -10,11 +10,11 @@ db = SQLAlchemy(app)
 iris_schema = {
     "type": "object",
     "properties": {
-        "sepal_length": {"type": "number", "multipleOf": 0.1},
-        "sepal_width": {"type": "number", "multipleOf": 0.1},
-        "petal_length": {"type": "number", "multipleOf": 0.1},
-        "petal_width": {"type": "number", "multipleOf": 0.1},
-        "flower_species": {"type": "integer"},
+        "sepal_length": {"type": "number", "multipleOf": 0.1, "exclusiveMinimum": 0},
+        "sepal_width": {"type": "number", "multipleOf": 0.1, "exclusiveMinimum": 0},
+        "petal_length": {"type": "number", "multipleOf": 0.1, "exclusiveMinimum": 0},
+        "petal_width": {"type": "number", "multipleOf": 0.1, "exclusiveMinimum": 0},
+        "flower_species": {"type": "integer", "minimum": 0},
     },
     "required": ["sepal_length", "sepal_width", "petal_length", "petal_width", "flower_species"]
 }
@@ -38,8 +38,20 @@ class Iris(db.Model):
                          self.flower_species})"
 
 
+def add_to_db(point):
+    db.session.add(point)
+    db.session.commit()
+
+
+def delete_by_id(record_id):
+    point = Iris.query.filter_by(id=record_id).first()
+    db.session.delete(point)
+    db.session.commit()
+    return point
+
+
 @app.route('/')
-def hello():
+def home_page():
     result = db.session.scalars(db.select(Iris))
     return render_template('home.html', objects=result)
 
@@ -47,47 +59,44 @@ def hello():
 @app.route('/add', methods=['POST', 'GET'])
 def add_data():
     if request.method == "POST":
-        with app.app_context():
-            new_obj = Iris(sepal_length=request.form['sepal_length'],
-                           sepal_width=request.form['sepal_width'],
-                           petal_length=request.form['petal_length'],
-                           petal_width=request.form['petal_width'],
-                           flower_species=request.form['flower_species'])
-            db.session.add(new_obj)
-            db.session.commit()
-        return "Record added"
+        try:
+            sepal_length = float(request.form['sepal_length'])
+            sepal_width = float(request.form['sepal_width'])
+            petal_length = float(request.form['petal_length'])
+            petal_width = float(request.form['petal_width'])
+            flower_species = int(request.form['flower_species'])
+
+            if (sepal_length <= 0 or
+                    sepal_width <= 0 or
+                    petal_length <= 0 or
+                    petal_width <= 0 or
+                    flower_species < 0):
+                raise ValueError
+        except ValueError:
+            return abort(400)
+        point = Iris(sepal_length=sepal_length,
+                     sepal_width=sepal_width,
+                     petal_length=petal_length,
+                     petal_width=petal_width,
+                     flower_species=flower_species)
+        add_to_db(point)
+        return redirect(url_for('home_page'))
     if request.method == "GET":
         return render_template('addition.html')
 
 
-@app.route('/delete/<record_id>', methods=['POST', 'GET'])
+@app.route('/delete/<record_id>', methods=['POST'])
 def delete_data(record_id):
-    with app.app_context():
-        point = Iris.query.filter_by(id=record_id).first()
-        db.session.delete(point)
-        db.session.commit()
-    return f"Deleted record ID: {record_id}"
-
-
-@app.route('/delete', methods=['POST'])
-def delete():
-    identity = request.form['to_remove'].split('=')[-1]
-    if identity:
-        if Iris.query.filter_by(id=identity).first() is not None:
-            return redirect(url_for('delete_data', record_id=identity))
-        else:
-            return abort(404)
+    if db.session.get(Iris, record_id) is not None:
+        delete_by_id(record_id)
+        return redirect(url_for("home_page"))
     else:
         return abort(404)
 
 
-def redirect_to_add():
-    return redirect('/add', code=302)
-
-
 @app.route('/api/data', methods=['GET'])
-def api_get_all():
-    data_points = Iris.query.all()
+def api_get_points():
+    data_points = db.session.scalars(db.select(Iris))
     data = [{'id': point.id,
              'sepal_length': point.sepal_length,
              'sepal_width': point.sepal_width,
@@ -110,46 +119,43 @@ def app_add_point():
                      flower_species=data.get('flower_species'))
         db.session.add(point)
         db.session.commit()
-        return jsonify({"Data added": point.id})
+        return {"Data added": point.id}, 302
     except ValidationError:
-        return {404: "Invalid data"}, 404
+        return {"Invalid data, the data point should follow the schema": iris_schema}, 400
 
 
 @app.route('/api/data/<int:record_id>', methods=['DELETE'])
 def api_delete_point(record_id):
-    try:
-        with app.app_context():
-            point = Iris.query.filter_by(id=record_id).first()
-            db.session.delete(point)
-            db.session.commit()
-        return jsonify({'primary key': point.id})
-    except:
-        return jsonify({404: "Record not found"})
+    if db.session.get(Iris, record_id) is not None:
+        delete_by_id(record_id)
+        return {'Data deleted': record_id}, 302
+    else:
+        return {"Record with following id not found": record_id}, 404
 
 
-@app.route('/test_get')
-def test_get():
-    response = requests.get('http://127.0.0.1:5000/api/data')
-    return response.json()
-
-
-@app.route('/test_post')
-def test_post():
-    point = {
-        "sepal_length": 1.0,
-        "sepal_width": 1.0,
-        "petal_length": 1.0,
-        "petal_width": 1.0,
-        "flower_species": 5,
-    }
-    response = requests.post('http://127.0.0.1:5000/api/data', json=point)
-    return response.json()
-
-
-@app.route('/test_delete')
-def test_delete():
-    response = requests.delete('http://127.0.0.1:5000/api/data/173')
-    return response.json()
+# @app.route('/test_get')
+# def test_get():
+#     response = requests.get('http://127.0.0.1:5000/api/data')
+#     return response.json()
+#
+#
+# @app.route('/test_post')
+# def test_post():
+#     point = {
+#         "sepal_length": 'abc',
+#         "sepal_width": 1.0,
+#         "petal_length": 1.0,
+#         "petal_width": 1.0,
+#         "flower_species": 5,
+#     }
+#     response = requests.post('http://127.0.0.1:5000/api/data', json=point)
+#     return response.json()
+#
+#
+# @app.route('/test_delete')
+# def test_delete():
+#     response = requests.delete('http://127.0.0.1:5000/api/data/222')
+#     return response.json()
 
 
 with app.app_context():
